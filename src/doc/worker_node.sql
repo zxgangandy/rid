55f254600fd3321905f18a527994a7bf968d69fa
zhaoxiaogang <286505491@qq.com>
2022-07-14 16:07:33 +0800

    sql for worker id;

---
+++ src/doc/worker_node.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 14/07/2022 16:06:18
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for worker_node
+-- ----------------------------
+DROP TABLE IF EXISTS `worker_node`;
+CREATE TABLE `worker_node` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'auto increment id',
+  `host_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'host name',
+  `port` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'port',
+  `worker_type` int NOT NULL COMMENT 'node type: CONTAINER(1), ACTUAL(2), FAKE(3)',
+  `launch_date` date NOT NULL COMMENT 'launch date',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'modified time',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created time',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_IDX_HOST_PORT` (`host_name`,`port`) USING BTREE COMMENT 'host和端口的唯一索引'
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='DB WorkerID Assigner for UID Generator';
+
+SET FOREIGN_KEY_CHECKS = 1;
